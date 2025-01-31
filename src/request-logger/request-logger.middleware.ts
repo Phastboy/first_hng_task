ebabff4892389e3a4b597e6afd4a6a4383816381
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-01-31 23:08:39 +0100

    create request logger middleware

---
+++ src/request-logger/request-logger.middleware.ts
@@ -0,0 +1,18 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class RequestLoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger(RequestLoggerMiddleware.name);
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl, body } = req;
+
+    this.logger.log(`Request: ${method} ${originalUrl}`);
+    if (body && typeof body === 'object' && Object.keys(body).length > 0) {
+      this.logger.log(`Body: ${JSON.stringify(body)}`);
+    }
+
+    next();
+  }
+}
